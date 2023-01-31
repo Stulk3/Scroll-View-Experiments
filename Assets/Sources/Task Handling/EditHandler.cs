cdc5cfc65e4b8e2f1235cbc8b4b4bab22ae7484b
Big Boss <stulk7746@gmail.com>
2023-01-31 17:31:19 +0300

    IMPORT: Project Files

---
+++ Assets/Sources/Task Handling/EditHandler.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EditHandler : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+
+    public void GetTaskValuesForEditMenu()
+    {
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+}
